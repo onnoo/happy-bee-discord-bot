cb06b6cd07b4f968eac5c33cb91d1e71645660e4
onnoo <sweyjw@gmail.com>
2020-11-07 21:06:24 +0900

    fix token

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix="!")
 bot_token = os.environ['BOT_TOKEN']
-riot_token = os.environ['X-RIOT-TOKEN']
+riot_token = os.environ['X_RIOT_TOKEN']
 
 
 @bot.event
